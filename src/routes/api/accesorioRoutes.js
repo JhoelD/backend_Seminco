a301d216a3973a78d8dfde90812c7b613d6c8684
Shoy09 <jhoel.dioses@tecsup.edu.pe>
2025-03-06 00:15:29 -0500

    ExplosivosApi3

--- src/routes/api/accesorioRoutes.js
+++ src/routes/api/accesorioRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const accesorioController = require('../../controllers/accesorioController');
+const verificarToken = require('../../middleware/auth');
 
-router.get('/', accesorioController.getAllAccesorios);
-router.get('/:id', accesorioController.getAccesorioById);
-router.post('/', accesorioController.createAccesorio);
-router.put('/:id', accesorioController.updateAccesorio);
-router.delete('/:id', accesorioController.deleteAccesorio);
+router.get('/', verificarToken, accesorioController.getAllAccesorios);
+router.get('/:id', verificarToken, accesorioController.getAccesorioById);
+router.post('/', verificarToken, accesorioController.createAccesorio);
+router.put('/:id', verificarToken, accesorioController.updateAccesorio);
+router.delete('/:id', verificarToken, accesorioController.deleteAccesorio);
 
 module.exports = router;
